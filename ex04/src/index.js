263233e95bdaa758f3ba7b05462d8cb29ce393dc
Niko Kostič <kostic.niko@icloud.com>
2022-04-07 20:37:35 +0200

    Multi-Dimensional Arrays

---
+++ ex04/src/index.js
@@ -0,0 +1,8 @@
+var temps = [
+    ["monday", 31, 32, 19, 37],
+    ["tuesday", 29, 27, 55, 36],
+    ["wednesday", 17, 27, 42, 46],
+    ["thursday", 29, 52, 21, 64],
+    ["friday", 91, 27, 31, 61],
+]
+console.log(temps);
