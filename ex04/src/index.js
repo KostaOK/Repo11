69c7dc900be5b904ca99f43749ee92c2fa3cc0b8
Niko Kostič <kostic.niko@icloud.com>
2022-04-07 20:39:41 +0200

    Multi-Dimensional Arrays

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -4,5 +4,10 @@ var temps = [
     ["wednesday", 17, 27, 42, 46],
     ["thursday", 29, 52, 21, 64],
     ["friday", 91, 27, 31, 61],
-]
+];
+
+function myArrayFunction(){
+    var averageDayTemp;
+    return averageDayTemp;
+}
 console.log(temps);
